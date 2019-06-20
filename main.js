d3062e2ba371624a0599055efec17654f2b8edd1
huang ping <766137195@qq.com>
2019-06-20 10:44:35 +0800

    '打开调试'

--- main.js
+++ main.js
@@ -11,41 +11,35 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    webPreferences: true
+    // {
+    //   preload: path.join(__dirname, 'preload.js')
+    // }
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
+  // 应用程序关闭时触发
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+    //当关闭应用程序应该删除该应用窗口对象
     mainWindow = null
   })
+    //打开调试
+  mainWindow.webContents.openDevTools()
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+//完成初始化时被触发，可通过app.isReady()方法检查此事件是否已触发
 app.on('ready', createWindow)
-
-// Quit when all windows are closed.
+//应用程序退出之前被触发
+app.on('before-quit',()=>{
+  alert('程序快退出了！');
+})
+// 当所有窗口都被关闭时退出应用
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
-
+//当应用程序首次启动或是重新激活都触发
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
 
